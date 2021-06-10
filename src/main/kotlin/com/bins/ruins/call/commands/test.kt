5a126dacb11dc6da8a21c7d6bc23e01de4592f63
A-Bins <yyangsuk33@naver.com>
2021-06-10 19:34:42 +0900

    No field with type net.minecraft.server.v1_16_R3.EnumItemSlot exists in class PacketPlayOutEntityEquipment. wtf

--- src/main/kotlin/com/bins/ruins/call/commands/test.kt
+++ src/main/kotlin/com/bins/ruins/call/commands/test.kt
@@ -1,18 +1,19 @@
 package com.bins.ruins.call.commands
 
-import com.bins.ruins.Ruins
-import com.bins.ruins.structure.objects.utilities.MetaReceiver.toCrossBowMeta
 import com.comphenix.protocol.PacketType
+import com.comphenix.protocol.ProtocolLib
 import com.comphenix.protocol.ProtocolLibrary
 import com.comphenix.protocol.events.PacketContainer
 import com.comphenix.protocol.wrappers.EnumWrappers
+import io.netty.util.internal.ThreadLocalRandom
+import net.minecraft.server.v1_16_R3.Material.WOOL
 import org.bukkit.Material
+import org.bukkit.Tag.WOOL
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.inventory.ItemStack
-import sun.audio.AudioPlayer.player
 import java.util.*
 
 
@@ -24,28 +25,36 @@ for(i in 0..54){
 val d = Drawer(*array.toTypedArray(), unlockState = 1)
 Strash(p.uniqueId, d)
 */
-
+//
+//val pm = ProtocolLibrary.getProtocolManager()
+//val packet = pm.createPacket(PacketType.Play.Server.ENTITY_EQUIPMENT)
+//packet.itemSlots.write(0, EnumWrappers.ItemSlot.MAINHAND)
+//packet.itemModifier.write(
+//0,
+//ItemStack(Material.CROSSBOW).apply {
+//    val meta = itemMeta.toCrossBowMeta()
+//    meta.addChargedProjectile(ItemStack(Material.ARROW))
+//    itemMeta = meta
+//}
+//)
+//packet.integers.write(0, p.entityId)
+//Ruins.instance.server.onlinePlayers.forEach {
+//    pm.sendServerPacket(it, packet);
+//}
 @Suppress("DEPRECATION")
 class test : CommandExecutor{
     override fun onCommand(p: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if(p is Player){
 
-            val pm = ProtocolLibrary.getProtocolManager()
-            val packet = pm.createPacket(PacketType.Play.Server.ENTITY_EQUIPMENT)
-            packet.itemSlots.write(0, EnumWrappers.ItemSlot.HEAD)
-            packet.itemModifier.write(
+            val packetContainer: PacketContainer = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.ENTITY_EQUIPMENT)
+            packetContainer.itemSlots.write(0, EnumWrappers.ItemSlot.HEAD)
+            packetContainer.itemModifier.write(
                 0,
-                ItemStack(Material.CROSSBOW).apply {
-                    val meta = itemMeta.toCrossBowMeta()
-                    meta.addChargedProjectile(ItemStack(Material.ARROW))
-                    itemMeta = meta
-                }
+                ItemStack(Material.WHITE_WOOL, 1, ThreadLocalRandom.current().nextInt(0, 10).toShort())
             )
-            packet.integers.write(0, p.entityId)
-            Ruins.instance.server.onlinePlayers.forEach {
-                pm.sendServerPacket(it, packet);
-            }
+            packetContainer.integers.write(0, p.entityId)
 
+            ProtocolLibrary.getProtocolManager().sendServerPacket (p, packetContainer)
 
         }
         return false
